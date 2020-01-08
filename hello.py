f37b1c9e17e3232544d9c97bd1b7e5b7d505d2dc
Alex Hultman <alexhultman@gmail.com>
2020-01-08 01:56:11 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,20 +1,27 @@
 # Questionable module name
 import uwebsocketspy as uWS
 
-def listenHandler():
-	print("Listening to port 3000")
-
-def getHandler(res, req):
-	res.end("Hello Python!")
-
-def postHandler(res, req):
-	res.end("Hello POST!");
-
+# the app
 app = uWS.App({
 	"some": "option"
 })
+
+# register http get
+def getHandler(res, req):
+	res.end("Hello Python!")
+
 app.get(getHandler)
+
+# register http post
+def postHandler(res, req):
+	res.end("Hello POST!")
+
 app.post(postHandler)
+
+# listen
+def listenHandler():
+	print("Listening to port 3000")
+
 app.listen(listenHandler)
 
 # Note: Will block asyncio for now, should integrate seamlessly with uvloop later on
